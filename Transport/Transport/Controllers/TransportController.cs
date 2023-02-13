4315c22c26ab27fd8aff5a766604ea864858e9cd
Kaloqn Todorov <k.todorov2019@pmg-pernik.com>
2023-02-13 09:53:03 +0200

    added zad 2

---
+++ Transport/Transport/Controllers/TransportController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Transport.Model;
+using Transport.Views;
+
+namespace Transport.Controllers
+{
+    public class TransportController
+    {
+        private Display display;
+        private Transportt transport;
+        public TransportController()
+        {
+            display = new Display();
+            transport = new Transportt(display.Km, display.Time);
+            display.TotalPrice = transport.CalculatePrice();
+            display.ShowPrice();
+        }
+    }
+}
---
+++ Transport/Transport/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Transport.Controllers;
+using Transport.Controllers;
+using Transport.Model;
+using Transport.Views;
+
+namespace Transport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TransportController transportController = new TransportController();
+        }
+    }
+}
